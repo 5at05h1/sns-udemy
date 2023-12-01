1fb03f1bcf81b4d2775bca5e3ce9553e800f8625
5at05h1 <69383363+5at05h1@users.noreply.github.com>
2023-12-01 17:33:56 +0900

    ユーザー新規登録API

--- api/server.js
+++ api/server.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
 
 const PORT = 5000;
+
+const prisma = new PrismaClient();
+
+// 新規ユーザー登録API
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+
+  return res.json({ user });
+});
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
