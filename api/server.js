d415748dc7f50e35ec5cace0b82687c3640c6cff
5at05h1 <69383363+5at05h1@users.noreply.github.com>
2023-12-04 10:30:05 +0900

    ユーザーログインAPI

--- api/server.js
+++ api/server.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const app = express();
-const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcrypt");
+const authRoute = require("./routers/auth");
+
+require("dotenv").config();
 
 const PORT = 5000;
 
-const prisma = new PrismaClient();
+app.use(express.json());
 
-// 新規ユーザー登録API
-app.post("/api/auth/register", async (req, res) => {
-  const { username, email, password } = req.body;
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password,
-    },
-  });
-
-  return res.json({ user });
-});
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
